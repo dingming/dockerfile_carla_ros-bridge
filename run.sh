57d86f8c4f76b06d1e0431f1aa4b34dfc2e1624f
dingming <dingming1980@gmail.com>
2020-10-06 09:38:13 +0900

    Remove --rm option

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ IMAGE=carlasim/carla-ros:local
 SHARED_DOCKER_DIR=/home/carla
 SHARED_HOST_DIR=$HOME/Program/carla/share_dir
 
-docker run -it --rm \
+docker run -it \
        --gpus all \
        -e DISPLAY=${DISPLAY} \
        --volume=$SHARED_HOST_DIR:$SHARED_DOCKER_DIR:rw \
